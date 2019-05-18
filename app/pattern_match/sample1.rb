21b898eabaf3a4bc717054bbd9c62df477fb0341
Sho Ito <i.sho.0628@gmail.com>
2019-05-18 23:44:10 +0900

    add sample codes to try out pattern match

---
+++ app/pattern_match/sample1.rb
@@ -0,0 +1,6 @@
+case { a: 0, b: 1 }
+  in { a: 0, x: 1 }
+    :unreachable
+  in { a: 0, b: var }
+    p var
+end
---
+++ app/pattern_match/sample2.rb
@@ -0,0 +1,8 @@
+require 'json'
+
+json =  File.read('/app/pattern_match/test_data.json')
+
+case JSON.parse(json, symbolize_names: true)
+  in { name: 'Alice', children: [{ name: 'Bob', age: age }]}
+    p age
+end
